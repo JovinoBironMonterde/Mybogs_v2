6c77461b8998be998f6e6f9c8a16aa18910ffa74
Jovino Monterde <jovinobironmonterde@gmail.com>
2024-06-22 14:33:52 +0800

    Done profile2

--- app/component/Appbar.jsx
+++ app/component/Appbar.jsx
@@ -1,9 +1,10 @@
 "use client"
-import React from 'react'
-import FollowButton from './FollowButton'
+import React from 'react';
+import FollowButton from './FollowButton';
 import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Drawer from '@mui/material/Drawer';
+import MenuIcon from '@mui/icons-material/Menu';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
@@ -11,13 +12,9 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
 import InboxIcon from '@mui/icons-material/Inbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
 import MailIcon from '@mui/icons-material/Mail';
-import Drawer from '@mui/material/Drawer';
-import MenuIcon from '@mui/icons-material/Menu';
 
-
-function appbar() {
+function Appbar() {
   const [open, setOpen] = React.useState(false);
 
   const toggleDrawer = (newOpen) => () => {
@@ -53,22 +50,22 @@ function appbar() {
       </List>
     </Box>
   );
+
   return (
     <div>
-      <Box sx={{p:1, display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+      <Box sx={{ p: 1, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
         <Box>
-          <Button onClick={toggleDrawer(true)}><MenuIcon/></Button>
+          <Button onClick={toggleDrawer(true)}><MenuIcon /></Button>
           <Drawer open={open} onClose={toggleDrawer(false)}>
             {DrawerList}
           </Drawer>
         </Box>
         <Box>
-          <FollowButton></FollowButton>
+          <FollowButton />
         </Box>
       </Box>
-     
     </div>
-  )
+  );
 }
 
-export default appbar
+export default Appbar;
